666d7fac20426f7d8cefe7a8f672c5892d3209d3
Keagan <keagan.rooyen@invisible.email>
2024-12-18 03:16:48 +0000

    Bug Fix: Fixed the Cross Origin Blocked error

--- src/components/StandardUI.js
+++ src/components/StandardUI.js
@@ -88,7 +88,7 @@ const StandardUI = () => {
 
     setLoading(true);
     try {
-      const res = await fetch(url);
+      const res = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`);
       if (!res.ok) {
         throw new Error(`Error fetching URL. Status: ${res.status}`);
       }
