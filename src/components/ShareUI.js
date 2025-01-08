85b384b63481f775f0564bc7e8b367813a51d46c
kvanrooyen-inv <keagan.rooyen@invisible.email>
2025-01-08 17:34:24 +0000

    Removed useEffect to fix deploy issue

--- src/components/ShareUI.js
+++ src/components/ShareUI.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '../components/ui/card';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
