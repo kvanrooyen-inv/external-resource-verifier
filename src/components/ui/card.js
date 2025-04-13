6f96c57ca3036cb5dfca151a43c353713df33613
Keagan <152187564+kvanrooyen-inv@users.noreply.github.com>
2025-04-13 23:52:40 +0100

    Revert "UI Refactor"

--- src/components/ui/card.js
+++ src/components/ui/card.js
@@ -1,30 +1,30 @@
-import React from "react";
+import React from 'react';
 
-export const Card = ({ children, className = "" }) => {
+export const Card = ({ children, className = '' }) => {
   return (
-    <div
-      className={`rounded-xl shadow-md max-w-xl bg-[#eff1f5] dark:bg-[#181825] ${className}`}
-    >
+    <div className={`rounded-xl shadow-md max-w-xl bg-[#eff1f5] dark:bg-[#181825] ${className}`}>
       {children}
     </div>
   );
 };
 
-export const CardHeader = ({ children, className = "" }) => {
-  return <div className={`p-4 ${className}`}>{children}</div>;
+export const CardHeader = ({ children, className = '' }) => {
+  return (
+    <div className={`p-4 ${className}`}>
+      {children}
+    </div>
+  );
 };
 
-export const CardTitle = ({ children, className = "" }) => {
+export const CardTitle = ({ children, className = '' }) => {
   return (
-    <h1
-      className={`text-4xl text-center mb-5 font-semibold text-[#4c4f69] dark:text-[#cdd6f4] ${className}`}
-    >
+    <h1 className={`text-4xl font-semibold text-[#4c4f69] dark:text-[#cdd6f4] ${className}`}>
       {children}
     </h1>
   );
 };
 
-export const CardContent = ({ children, className = "" }) => {
+export const CardContent = ({ children, className = '' }) => {
   return (
     <div className={`p-4 text-[#4c4f69] dark:text-[#cdd6f4] ${className}`}>
       {children}
