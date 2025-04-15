b09344fd91780a9461650c00527622643720e06a
Keagan <keagan.rooyen@invisible.email>
2025-04-15 21:41:30 +0100

    More clean up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ThemeToggle from './components/ThemeToggle';
-import Dashboard from './components/Dashboard';
-import ReportPage from './components/ReportPage';
-import NotificationSystem from './components/NotificationSystem.js';
+import React from "react";
+import ThemeToggle from "./components/ThemeToggle";
+import Dashboard from "./components/Dashboard";
+import NotificationSystem from "./components/NotificationSystem.js";
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-background-color text-text-color">
-        <ThemeToggle />
-        <NotificationSystem />
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/report/:reportId" element={<ReportPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="min-h-screen bg-background-color text-text-color">
+      <ThemeToggle />
+      <NotificationSystem />
+      <Dashboard />
+    </div>
   );
 }
 
